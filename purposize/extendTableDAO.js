97a6478657b02141fac24b18a4df854620e74655
Marcel Reppenhagen <marcelreppi@gmail.com>
2018-06-15 15:00:03 +0200

    switch to official sequelize hooks

--- purposize/extendTableDAO.js
+++ purposize/extendTableDAO.js
@@ -1,22 +1,19 @@
-module.exports = (tableDAO) => {
-  const originalCreate = tableDAO.create 
+const sequelize = require('sequelize')
 
-  tableDAO.create = function() {
-    const values = arguments['0']
-    const options = arguments['1']
+module.exports = (tableDAO, metaDataTable, purposizeTables) => {
 
+  tableDAO.hook('beforeCreate', async (values, options) => {
     if (typeof options === 'undefined' || typeof options.purpose === 'undefined') {
-      return Promise.reject('ERROR: Please specify a purpose when creating a new instance!')
+      return sequelize.Promise.reject(new Error('Please specify a purpose when creating a new instance!'))
     }
-    
     if (typeof options.purpose === 'string' || Array.isArray(options.purpose)) {
       // TODO: 
       // 1. Check if values are allowed to be stored for the specified purpose
       // 2. Store which fields are being stored for which purpose in metadata tables
       // Maybe more?
-      return originalCreate.apply(this, arguments)
     } else {
-      return Promise.reject('ERROR: Incorrect purpose format!')
-    }  
-  }
+      return sequelize.Promise.reject(new Error("Incorrect purpose format!"))
+    }
+
+  })
 }
