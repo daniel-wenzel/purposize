0d3156c257d6760f30f3b92d5e690b5545dd6940
Marcel Reppenhagen <marcelreppi@gmail.com>
2018-06-18 13:22:57 +0200

    added precheck for sensitive data values

--- purposize/extendTableDAO.js
+++ purposize/extendTableDAO.js
@@ -1,13 +1,40 @@
 const sequelize = require('sequelize')
 
-module.exports = (tableDAO, metaDataTable, purposizeTables) => {
+module.exports = (tableDAO, metaDataPurposeTable, purposizeTables) => {
 
   const originalCreate = tableDAO.create 
   tableDAO.create = async function() {
     const values = arguments['0']
     const options = arguments['1']
+
+    const givenFields = Object.keys(values)
+
+    // Get all sensitive values for this table
+    let personalDataFields = await purposizeTables.personalDataFields.findAll({
+      where: {
+        tableName: tableDAO.tableName
+      }
+    })
+    personalDataFields = personalDataFields.map(r => r.dataValues.fieldName)
+
+    // Check if the given data fields contain personal data 
+    const sensitiveDataFields = [] // Filtering the personal data fields and store them here
+    for (let i = 0, len = givenFields.length; i < len; i++) {
+      const givenField = givenFields[i]
+      if (personalDataFields.some( f => f === givenField)) {
+        sensitiveDataFields.push(givenField)
+      }
+    }
+
+    // If the given data fields do not contain any personal data execute original define
+    if (sensitiveDataFields.length === 0) {
+      return originalCreate.apply(this, arguments)
+    }
+
+    // Personal data was given as data fields
+    // Now check if purpose was given and if they are compatible
     if (typeof options === 'undefined' || typeof options.purpose === 'undefined') {
-      return sequelize.Promise.reject(new Error('Please specify a purpose when creating a new instance!'))
+      return sequelize.Promise.reject(new Error('Please specify a purpose when creating a new instance that contains personal data!'))
     }
     if (typeof options.purpose === 'string' || Array.isArray(options.purpose)) {
       // TODO: 
@@ -16,21 +43,20 @@ module.exports = (tableDAO, metaDataTable, purposizeTables) => {
       // Maybe more?
 
       const purposes = [].concat(options.purpose) 
-      console.log(purposes) 
 
       // Get all fields that are allow for the specified purpose(s)
       const purposeResult = await purposizeTables.purposeDataFields.findAll({
         where: {
-          purpose: options.purpose,
+          purpose: purposes,
           tableName: tableDAO.tableName
         }
       })
       const allowedFields = purposeResult.map(p => p.dataValues.fieldName)
-      const keyArray = Object.keys(values)
-      for (let i = 0, len = keyArray.length; i < len; i++) {
-        const userField = keyArray[i]
-        if (!allowedFields.some( f => f === userField)) {
-          return sequelize.Promise.reject(new Error(`Field "${userField}" is incompatible with purpose(s): ${options.purpose}`))
+      // Check if the given fields are
+      for (let i = 0, len = sensitiveDataFields.length; i < len; i++) {
+        const givenField = sensitiveDataFields[i]
+        if (!allowedFields.some( f => f === givenField)) {
+          return sequelize.Promise.reject(new Error(`Field "${givenField}" is incompatible with purpose(s): ${purposes.join(', ')}`))
         }
       }
 
