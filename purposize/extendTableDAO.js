eeb0eb800c7cfc69231daf2bcd00b01000a60091
Marcel Reppenhagen <marcelreppi@gmail.com>
2018-06-27 22:47:00 +0200

    use find method for find() and findAll()

--- purposize/extendTableDAO.js
+++ purposize/extendTableDAO.js
@@ -1,8 +1,4 @@
-const sequelize = require('sequelize')
-const Op = sequelize.Op
-
 const purposizeCreate = require('./methods/create')
-const purposizeFindAll = require('./methods/findAll')
 const purposizeFind = require('./methods/find')
 
 module.exports = (tableDAO, metaDataPurposeTable, purposizeTables) => {
@@ -14,7 +10,7 @@ module.exports = (tableDAO, metaDataPurposeTable, purposizeTables) => {
 
   const originalFindAll = tableDAO.findAll
   tableDAO.findAll = async function() {
-    return await purposizeFindAll(arguments, originalFindAll, tableDAO, metaDataPurposeTable, purposizeTables)
+    return await purposizeFind(arguments, originalFindAll, tableDAO, metaDataPurposeTable, purposizeTables)
   }
 
   const originalFind = tableDAO.find
