005b366d3f79012642fa5e976ec6de28f8e0e3b3
Marcel Reppenhagen <marcelreppi@gmail.com>
2018-07-02 13:11:33 +0200

    added more update test

--- tests/update.test.js
+++ tests/update.test.js
@@ -6,8 +6,8 @@ const chai = require('chai')
 const expect = chai.expect
 
 let Customers
-describe('Testing update through instance.save method', () => {
-  before(async () => {
+describe.only('Testing update through instance.save method', () => {
+  beforeEach(async () => {
     await sequelize.getQueryInterface().dropAllTables()
     Customers = sequelize.define(tableName, tableDefinition);
     await sequelize.sync()
@@ -87,7 +87,7 @@ describe('Testing update through instance.save method', () => {
     try {
       const carl = await Customers.find({ 
         where: {
-          postalAddress: "9876 Berlin"
+          postalAddress: "1234 Cheapcity"
         },
         for: 'FULFILLMENT'
       })
@@ -105,7 +105,7 @@ describe('Testing update through instance.save method', () => {
   it('Successful update with adding new data fields', async () => {
     const carl = await Customers.find({ 
       where: {
-        postalAddress: "9876 Berlin"
+        postalAddress: "1234 Cheapcity"
       },
       for: 'FULFILLMENT'
     })
@@ -119,17 +119,166 @@ describe('Testing update through instance.save method', () => {
     expect(oldCarlPurposes.length).to.equal(1)
 
     carl.eMail = "carl@email.com"
-    await carl.save({
+    let newCarl = await carl.save({
       purpose: 'NEWSLETTER'
     })
+    expect(newCarl).not.to.be.null
+    expect(newCarl.postalAddress).to.be.undefined
+    expect(newCarl.eMail).to.equal("carl@email.com")
 
-    const newCarl = await Customers.find({ 
+    newCarl = await Customers.find({ 
       where: {
         eMail: "carl@email.com"
       },
       for: 'NEWSLETTER'
     })
     expect(newCarl).not.to.be.null
+    expect(newCarl.postalAddress).to.be.undefined
+    expect(newCarl.eMail).to.equal("carl@email.com")
+
+    const newCarlPurposes = await sequelize.model('purposize_customersPurposes').findAll({
+      where: { 
+        customersId: newCarl.id
+      }
+    })
+    // Check that carl is now stored for two purposes: FULFILLMENT and NEWSLETTER
+    expect(newCarlPurposes.length).to.equal(2)
+    expect(newCarlPurposes.map( p => p.purpose ).includes('NEWSLETTER')).to.equal(true)
+  })
+})
+
+describe.only('Testing update through instance.update method', () => {
+  beforeEach(async () => {
+    await sequelize.getQueryInterface().dropAllTables()
+    Customers = sequelize.define(tableName, tableDefinition);
+    await sequelize.sync()
+    await purposize.loadPurposes('./purposes.yml')
+    
+    const alice = await Customers.create({
+      eMail: "alice@email.com",
+      postalAddress: "1234 Shoppington",
+      unfulfilledOrders: 1
+    }, {
+      purpose: 'ORDER'
+    })
+
+    const bob = await Customers.create({
+      eMail: "bob@email.com",
+      postalAddress: "1234 Buytown",
+      unfulfilledOrders: 2
+    }, {
+      purpose: ['ORDER', 'NEWSLETTER']
+    })
+
+    const carl = await Customers.create({
+      // eMail: "carl@email.com",
+      postalAddress: "1234 Cheapcity",
+    }, {
+      purpose: 'FULFILLMENT'
+    })
+  })
+
+  it('Successful update with adding new data fields', async () => {
+    const carl = await Customers.find({ 
+      where: {
+        postalAddress: "1234 Cheapcity"
+      },
+      for: 'FULFILLMENT'
+    })
+
+    const oldCarlPurposes = await sequelize.model('purposize_customersPurposes').findAll({
+      where: { 
+        customersId: carl.id
+      }
+    })
+    // Check that carl was only stored for the purpose FULFILLMENT
+    expect(oldCarlPurposes.length).to.equal(1)
+
+    await carl.update({
+      postalAddress: "9876 Berlin"
+    })
+
+    const oldCarl = await Customers.find({ 
+      where: {
+        postalAddress: "1234 Cheapcity"
+      },
+      for: 'FULFILLMENT'
+    })
+    expect(oldCarl).to.be.null
+
+    const newCarl = await Customers.find({ 
+      where: {
+        postalAddress: "9876 Berlin"
+      },
+      for: 'FULFILLMENT'
+    })
+
+    expect(newCarl).not.to.be.null
+    expect(newCarl.postalAddress).to.equal("9876 Berlin")
+
+    const newCarlPurposes = await sequelize.model('purposize_customersPurposes').findAll({
+      where: { 
+        customersId: carl.id
+      }
+    })
+    // Check that carl is still only stored for the purpose FULFILLMENT
+    expect(newCarlPurposes.length).to.equal(1)
+  })
+
+  it('Error when adding a new illegal field and saving', async () => {
+    try {
+      const carl = await Customers.find({ 
+        where: {
+          postalAddress: "1234 Cheapcity"
+        },
+        for: 'FULFILLMENT'
+      })
+  
+      await carl.update({
+        eMail: "carl@email.com"
+      })
+
+    } catch (error) {
+      expect(error).to.be.instanceOf(Error)
+      return
+    }
+    expect.fail(null, null, 'No error was thrown')
+  })
+
+  it('Successful update with adding new data fields', async () => {
+    const carl = await Customers.find({ 
+      where: {
+        postalAddress: "1234 Cheapcity"
+      },
+      for: 'FULFILLMENT'
+    })
+
+    const oldCarlPurposes = await sequelize.model('purposize_customersPurposes').findAll({
+      where: { 
+        customersId: carl.id
+      }
+    })
+    // Check that carl was only stored for the purpose FULFILLMENT
+    expect(oldCarlPurposes.length).to.equal(1)
+
+    let newCarl = await carl.update({
+      eMail: "carl@email.com"
+    }, {
+      purpose: 'NEWSLETTER'
+    })
+
+    expect(newCarl).not.to.be.null
+    expect(newCarl.postalAddress).to.be.undefined
+    expect(newCarl.eMail).to.equal("carl@email.com")
+
+    newCarl = await Customers.find({ 
+      where: {
+        eMail: "carl@email.com"
+      },
+      for: 'NEWSLETTER'
+    })
+    expect(newCarl).not.to.be.null
+    expect(newCarl.postalAddress).to.be.undefined
     expect(newCarl.eMail).to.equal("carl@email.com")
 
     const newCarlPurposes = await sequelize.model('purposize_customersPurposes').findAll({
